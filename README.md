7baf9800387b0bae3c165d2bd3c91d1760d55628
andrew <andrew.b.suzuki@gmail.com>
2014-03-20 19:05:09 -0400

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## Vessel
+
+Vessel is a simple, yet powerful file-based CMS (content management system). Written in PHP, built on Laravel.
+
+Currently 
+
+### License
+
+Vessel is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
---
+++ src/Hokeo/Vessel/VesselServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Hokeo\Vessel;
+
+use Illuminate\Support\ServiceProvider;
+
+class VesselServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('hokeo/vessel');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
