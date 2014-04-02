87e55b13e9f0bba1a978f51c00e1cde4a694c310
andrew <andrew.b.suzuki@gmail.com>
2014-04-01 21:46:47 -0400

    travis fix 11

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -22,13 +22,38 @@ else
 	file_put_contents($nudir.'/app/config/testing/database.php', "<?php return array('connections' => array('mysql' => array('database' => 'vessel_testing', 'username' => 'root', 'password' => 'root')));");
 }
 
-exec('php artisan migrate:reset --env=testing');
-exec('php artisan migrate --bench=hokeo/vessel --env=testing');
-exec('php artisan db:seed --class=Hokeo\\\Vessel\\\Seeds\\\TestVesselSeeder --env=testing');
+// clear test db and seed
+
+print "Running artisan database commands for testing...\n";
+
+passthru('php artisan migrate:reset --env=testing');
+passthru('php artisan migrate --bench=hokeo/vessel --env=testing');
+passthru('php artisan db:seed --class=Hokeo\\\Vessel\\\Seeds\\\TestVesselSeeder --env=testing');
 
 print 'Changing back to '.$cwd."\n";
 chdir($cwd);
 
-require_once __DIR__.'/../vendor/autoload.php';
+// The below is a terrible hack to circumvent function redeclaration errors of the crypt_random_string() function in the below file
+// , which for whatever reason they don't want to surround with function_exists().
 
-return array('connections' => array('mysql' => array('database' => 'vessel_testing', 'username' => 'root', 'password' => '')));
+$evil_file = __DIR__.'/../vendor/phpseclib/phpseclib/phpseclib/Crypt/Random.php';
+
+print 'Hackily-editing: '.$evil_file."\n";
+
+if (file_exists($evil_file))
+{
+	$evil_file_contents = file_get_contents($evil_file);
+	$evil_file_nustring = '<?php /* VESSEL TESTING HACK (from tests/bootstrap.php) */ if (defined("CRYPT_RANDOM_IS_WINDOWS") || function_exists("crypt_random_string")) { return; }';
+
+	if (strpos($evil_file_contents, $evil_file_nustring) === false)
+	{
+		// find first occurrence of <?php and replace with our $nustring
+		file_put_contents($evil_file, implode($evil_file_nustring, explode('<?php', $evil_file_contents, 2)));
+	}
+}
+
+print 'Requiring autoloader...'."\n";
+
+// actual bootstrap:
+
+require_once __DIR__.'/../vendor/autoload.php';
